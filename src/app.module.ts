05ea190c209c4f30e9bc74b7a6dcf324d076f997
Kamil Domagała <k.domagala@getprintbox.com>
2024-07-22 18:54:24 +0200

    Adds database module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
 import { ExercisesModule } from './exercises/exercises.module';
 import { SetsModule } from './sets/sets.module';
 import { WorkoutsModule } from './workouts/workouts.module';
 
 @Module({
-  imports: [ExercisesModule, SetsModule, WorkoutsModule],
+  imports: [ExercisesModule, SetsModule, WorkoutsModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })

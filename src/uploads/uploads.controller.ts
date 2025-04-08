e55058b2899a5b4e9e2da6baddf7bc8852f54165
lizaq94 <domagala.k94@gmail.com>
2025-04-08 21:48:07 +0200

    feat: add controller for upload image

--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Express } from 'express';
+import { ApiHeaders, ApiOperation } from '@nestjs/swagger';
+import { UploadsService } from './providers/uploads.service';
 
 @Controller('uploads')
-export class UploadsController {}
+export class UploadsController {
+  constructor(private readonly uploadService: UploadsService) {}
+  @UseInterceptors(FileInterceptor('image'))
+  @ApiHeaders([
+    { name: 'Content-Type', description: 'multipart/form-data' },
+    { name: 'Authorization', description: 'Bearer Token' },
+  ])
+  @ApiOperation({ summary: 'Upload a nwe image to the server' })
+  @Post('image')
+  public uploadImage(@UploadedFile() file: Express.Multer.File) {
+    return this.uploadService.uploadImage(file);
+  }
+}

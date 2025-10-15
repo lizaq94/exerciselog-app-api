8cfed22307fbcdc32b3bda52f3fe52b1bdf9ab52
lizaq94 <domagala.k94@gmail.com>
2025-10-15 21:14:29 +0200

    feat: enhance `AiService` with response parsing and transformation, integrate logger for improved debugging, and update module dependencies

--- src/ai/ai.module.ts
+++ src/ai/ai.module.ts
@@ -5,10 +5,16 @@ import { AiService } from './providers/ai.service';
 import { OpenRouterProvider } from './providers/open-router.provider';
 import { LoggerModule } from '../logger/logger.module';
 import { AiResponseParserService } from './services/ai-response-parser/ai-response-parser.service';
+import { AiResponseTransformerService } from './services/ai-response-transformer/ai-response-transformer.service';
 
 @Module({
   imports: [HttpModule, LoggerModule],
   controllers: [AiController],
-  providers: [AiService, OpenRouterProvider, AiResponseParserService],
+  providers: [
+    AiService,
+    OpenRouterProvider,
+    AiResponseParserService,
+    AiResponseTransformerService,
+  ],
 })
 export class AiModule {}
--- src/ai/providers/ai.service.ts
+++ src/ai/providers/ai.service.ts
@@ -1,17 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { GenerateWorkoutDto } from '../dto/generate-workout.dto';
 import { OpenRouterProvider } from './open-router.provider';
+import { AiResponseParserService } from '../services/ai-response-parser/ai-response-parser.service';
+import { AiResponseTransformerService } from '../services/ai-response-transformer/ai-response-transformer.service';
+import { LoggerService } from '../../logger/logger.service';
+import { CreateWorkoutBulkDto } from '../../workouts/dto/bulk';
 
 @Injectable()
 export class AiService {
-  constructor(private readonly openRouterProvider: OpenRouterProvider) {}
+  constructor(
+    private readonly openRouterProvider: OpenRouterProvider,
+    private readonly parseService: AiResponseParserService,
+    private readonly transformerService: AiResponseTransformerService,
+    private readonly logger: LoggerService,
+  ) {}
 
-  async generateWorkout(generateWorkoutDto: GenerateWorkoutDto) {
+  async generateWorkout(
+    generateWorkoutDto: GenerateWorkoutDto,
+  ): Promise<CreateWorkoutBulkDto[]> {
     try {
-      const result =
+      const rawResponse =
         await this.openRouterProvider.generateWorkout(generateWorkoutDto);
+      const validateResponse = await this.parseService.parse(rawResponse);
+      const workoutPlans =
+        this.transformerService.transformToAppFormat(validateResponse);
 
-      return result;
+      this.logger.log(
+        `Successfully generated ${workoutPlans.length} workout plans`,
+      );
+
+      console.log('Kamil | workoutPlans: ', workoutPlans);
+      return workoutPlans;
     } catch (error) {
       const contextualError = new Error(
         `Failed to generate workout for goal "${generateWorkoutDto.goal}": ${error.message}`,

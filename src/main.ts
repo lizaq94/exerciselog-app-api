6edfa76ce5ea8e4bfadc7363c579c893a63e733b
lizaq94 <domagala.k94@gmail.com>
2026-05-26 20:48:08 +0200

    refactor: use ConfigService to dynamically set app port in bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { createApp } from './app.create';
+import ConfigService from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   createApp(app);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+  const port = configService.getAppConfig().port;
+
+  await app.listen(port);
 }
 bootstrap();

82bdc4d7cc403961e88b1c1a7a77993354bf0c80
lizaq94 <domagala.k94@gmail.com>
2026-05-29 22:31:27 +0200

    refactor: conditionally load Swagger only in non-production environments in `createApp` function

--- src/app.create.ts
+++ src/app.create.ts
@@ -7,18 +7,21 @@ import { LoggerService } from './logger/logger.service';
 import ConfigService from './config/config.service';
 
 export function createApp(app: INestApplication): void {
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle('Exerciselog')
-    .setDescription(
-      'API for managing exercise logs, including creating, updating, and retrieving workout sessions. Supports JWT-based authentication for secure access.',
-    )
-    .setVersion('0.1')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, document);
   const configService = app.get(ConfigService);
   const { nodeEnv, corsOrigin } = configService.getAppConfig();
+
+  if (nodeEnv !== 'production') {
+    const swaggerConfig = new DocumentBuilder()
+      .setTitle('Exerciselog')
+      .setDescription(
+        'API for managing exercise logs, including creating, updating, and retrieving workout sessions. Supports JWT-based authentication for secure access.',
+      )
+      .setVersion('0.1')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('api', app, document);
+  }
 
   app.use(cookieParser());
   app.useGlobalPipes(
